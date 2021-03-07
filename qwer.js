4b29fd9fc6afa6e795ed82dd1c42b5a73b5a9765
eunchannam <75837025+eunchannam@users.noreply.github.com>
2021-03-08 02:25:47 +0900

    Update qwer.js

--- qwer.js
+++ qwer.js
@@ -7,18 +7,18 @@ show.innerHTML = money;
 for (var i=0; i < use.length; i++){
   use[i].addEventListener('click', use_money);
   add[i].addEventListener('click', add_money);
-  if (Number(use[i].id) < money){
+  if (Number(use[i].id) <= money){
     use[i].style.color = "blue";
   }
   else use[i].style.color = "red";
 }
 
 function use_money(){
-  if(Number(this.id) < money){
+  if(Number(this.id) <= money){
     money -= Number(this.id);
 }
   for(var i=0; i < use.length; i++){
-    if (Number(use[i].id) < money){
+    if (Number(use[i].id) <= money){
       use[i].style.color = "blue";
     }
     else use[i].style.color = "red";
@@ -28,7 +28,7 @@ function use_money(){
 
 function add_money(){
   money += Number(this.id);
-  for(var i=0; i < use.length; i++){
+  for(var i=0; i <= use.length; i++){
     if (Number(use[i].id) < money){
       use[i].style.color = "blue";
     }
